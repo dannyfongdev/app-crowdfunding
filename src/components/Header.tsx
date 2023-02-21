29ba5a21eabc50012f9f68fc3e1c628e1d13b937
Danny Fong <danny.fong.dev@outlook.com>
2023-02-20 20:43:06 -0800

    Bookmark state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import styles from "./Header.module.css";
 
 interface HeaderProps {
   onBackThisProject: () => void;
 }
 
-// @todo bookmark hover, select
+// @todo bookmark hover effect
+// @todo bookmarkLabel text alignment
+
 
 export default function Header({ onBackThisProject }: HeaderProps): JSX.Element {
+  const [bookmarked, setBookmarked] = useState(false)
+
+  const handleBookmarkClick = () => {
+    setBookmarked(!bookmarked);
+  }
+
   return (
     <div className={styles.container}>
       <img className={styles.logo} src="./images/logo-mastercraft.svg" alt="logo" />
@@ -16,9 +25,9 @@ export default function Header({ onBackThisProject }: HeaderProps): JSX.Element 
       </p>
       <div className={styles.actions}>
         <button className={styles.btn} onClick={onBackThisProject}>Back this project</button>
-        <div className={styles.bookmarkWrapper}>
-          <img className={styles.bookmarkIcon} src="./images/icon-bookmark.svg" />
-          <span className={styles.bookmarkLabel}>Bookmark</span>
+        <div className={styles.bookmarkWrapper} onClick={handleBookmarkClick}>
+          <img className={styles.bookmarkIcon} src={!bookmarked ? "./images/icon-bookmark.svg" : "./images/icon-bookmarked.svg"} alt="bookmark icon"/>
+          <span className={styles.bookmarkLabel}>{bookmarked ? 'Bookmarked' : 'Bookmark'}</span>
         </div>
         
       </div>
